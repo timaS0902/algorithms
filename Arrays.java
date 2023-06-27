79b3f7780eab573af6867133fdeb3491026816bb
tima_soldatov <86138803+timaS0902@users.noreply.github.com>
2023-06-27 17:49:07 +0500

    Array algorithm in java

---
+++ Arrays.java
@@ -0,0 +1,73 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		int[] arr = {1,2,3,4,5};
+		
+/////////////////////AVERAGE OF ARRAY/////////////////		
+//													//
+//		int N = a.length;							//
+//		double sum = 0.0;							//
+//		for(int i = 0; i<N; i++)					//
+//		{											//
+//			sum+= a[i];								//
+//		}											//
+//		double average = sum/N; 					//
+//													//
+//		System.out.println("Average = " + average);	//
+//////////////////////////////////////////////////////
+
+		
+//		MAX VALUE OF ARRAY
+//					
+//		double max = arr[0];
+//		for(int i = 1; i<arr.length; i++)
+//		{
+//			if(arr[i]>max)
+//			{
+//				max = arr[i];
+//			}
+//		}
+//		System.out.println("Max = " + max);
+	
+		
+//		COPY TO ANOTHER ARRAY 
+//	
+//		int N = arr.length;
+//		double []b = new double[N];
+//		for(int i = 0; i < N; i++)
+//		{
+//			b[i] = arr[i];
+//		}
+//		System.out.print("Copy of arr: ");
+//		for(int k = 0; k < b.length; k++)
+//		{
+//			System.out.print(b[k] + " ");
+//		}
+
+		
+		//REVERSE ARRAY 
+//		System.out.print("Original Array: ");
+//		for(int k = 0; k < arr.length; k++)
+//		{
+//			System.out.print(arr[k] + " ");
+//		}
+//		
+//		int N = arr.length;
+//		for(int i = 0; i < N/2; i++)
+//		{
+//			int temp = arr[i];
+//			arr[i] = arr[N-1-i];
+//			arr[N-i-1] = temp;
+//		}
+//		System.out.print("\nReverse Array: ");
+//		for(int m = 0; m < arr.length; m++)
+//		{
+//			System.out.print(arr[m] + " ");
+//		}
+		
+	}
+
+}
